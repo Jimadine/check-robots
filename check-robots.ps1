bd461c3f5e99f42667cc602d3a586085d65c0351
Jim Adamson <jim.adamson@york.ac.uk>
2025-09-15 15:06:51 +0100

    Improve script formatting

--- check-robots.ps1
+++ check-robots.ps1
@@ -8,25 +8,23 @@
 #>
 $domainsFile = $args[0]
 If (-not $args[0]) {
-  "Filepath argument not supplied"
-  Return
+    "Filepath argument not supplied"
+    Return
 }
 
-ForEach($domain in (Get-Content -Path $domainsFile) ) {
-    try
-    {   
+ForEach ($domain in (Get-Content -Path $domainsFile) ) {
+    Try {
         $Request = "https://${domain}/robots.txt"
         $Response = Invoke-WebRequest -URI $Request
         # This will only execute if the Invoke-WebRequest is successful.
         $StatusCode = $Response.StatusCode
     }
-    catch
-    {
+    Catch {
         $StatusCode = $_.Exception.Response.StatusCode.value__
     }
 
-    If ($StatusCode -eq '200'){ "${domain} has a robots.txt (HTTP Status Code ${StatusCode})" } 
-    Elseif ($StatusCode -eq '404'){ Write-Host -ForegroundColor Red "${domain} does NOT have a robots.txt (HTTP Status Code ${StatusCode})" } 
+    If ($StatusCode -eq '200') { "${domain} has a robots.txt (HTTP Status Code ${StatusCode})" } 
+    Elseif ($StatusCode -eq '404') { Write-Host -ForegroundColor Red "${domain} does NOT have a robots.txt (HTTP Status Code ${StatusCode})" } 
     Else { "${domain} returned a non-200 status code (${StatusCode})" }
 
 }
